fc92d2db6f659418564605b0eb6335eda173dd02
hankelc <taichen.liu@imorpheus.io>
2020-03-27 10:57:26 +0800

    add content into ifps db

--- cmd/tau-graphsync/createnode.go
+++ cmd/tau-graphsync/createnode.go
@@ -5,11 +5,12 @@ import (
 	"context"
 	"fmt"
 	"io"
-	"sync"
 
 	"github.com/ipld/go-ipld-prime/fluent"
+	blocks "github.com/ipfs/go-block-format"
 	cid "github.com/ipfs/go-cid"
 	cidlink "github.com/ipld/go-ipld-prime/linking/cid"
+	ipfs "github.com/ipfs/go-ipfs/lib"
 	ipld "github.com/ipld/go-ipld-prime"
 	ipldfree "github.com/ipld/go-ipld-prime/impl/free"
 	mh "github.com/multiformats/go-multihash"
@@ -27,15 +28,20 @@ func createMapNode(ctx context.Context) (ipld.Link, error) {
 
 	//node -> link
 	linkBuilder := cidlink.LinkBuilder{Prefix: cid.NewPrefixV1(cid.DagCBOR, mh.SHA2_256)}
-	blocksWritten := make(map[ipld.Link][]byte)
-	var storeLk sync.RWMutex
+	iNode, _:= ipfs.Node()
+	bStore := iNode.Blockstore
 	storer := func(lnkCtx ipld.LinkContext) (io.Writer, ipld.StoreCommitter, error) {
         var buffer bytes.Buffer
         committer := func(lnk ipld.Link) error {
-            storeLk.Lock()
-            blocksWritten[lnk] = buffer.Bytes()
-            storeLk.Unlock()
-            return nil
+			asCidLink, ok := lnk.(cidlink.Link)
+			if !ok {
+                return fmt.Errorf("Unsupported Link Type")
+            }
+            block, err := blocks.NewBlockWithCid(buffer.Bytes(), asCidLink.Cid)
+            if err != nil {
+                return err
+            }
+            return bStore.Put(block)
         }
         return &buffer, committer, nil
     }
