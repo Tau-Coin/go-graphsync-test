f94471b5a215a317554a1b71da107c09ff990887
shengxianshui <shengxianshui@imorpheus.io>
2020-03-28 17:01:59 +0800

    Try another selector for hamt

--- cmd/tau-graphsync/createselector.go
+++ cmd/tau-graphsync/createselector.go
@@ -4,26 +4,26 @@ import (
 	"github.com/ipld/go-ipld-prime/traversal/selector/builder"
 	ipld     "github.com/ipld/go-ipld-prime"
 	ipldfree "github.com/ipld/go-ipld-prime/impl/free"
-	ipldselector "github.com/ipld/go-ipld-prime/traversal/selector"
-	//selectorbuilder "github.com/ipld/go-ipld-prime/traversal/selector/builder"
+	selectorbuilder "github.com/ipld/go-ipld-prime/traversal/selector/builder"
 )
 
 // state hamt tree
 func stateSelector() ipld.Node {
 	ssb := builder.NewSelectorSpecBuilder(ipldfree.NodeBuilder())
-    //return ssb.ExploreAll(ssb.Matcher()).Node()
-    /*
-    return ssb.ExploreIndex(1, ssb.ExploreFields(func(efsb selectorbuilder.ExploreFieldsSpecBuilder) {
+	//return ssb.ExploreAll(ssb.Matcher()).Node()
+	/*
+	return ssb.ExploreIndex(1, ssb.ExploreFields(func(efsb selectorbuilder.ExploreFieldsSpecBuilder) {
 		//efsb.Insert("0", ssb.ExploreAll(ssb.Matcher()))
 		//efsb.Insert("0", ssb.Matcher())
-		efsb.Insert("0", ssb.ExploreFields(func(efsb1 selectorbuilder.ExploreFieldsSpecBuilder) {
-			efsb1.Insert("/", ssb.ExploreAll(ssb.Matcher()))
-		}))
 	})).Node()
 	*/
 
-	return ssb.ExploreRecursive(ipldselector.RecursionLimitNone(),
-		ssb.ExploreAll(ssb.ExploreRecursiveEdge())).Node()
+	return ssb.ExploreIndex(1, ssb.ExploreAll(
+			ssb.ExploreFields(func(efsb selectorbuilder.ExploreFieldsSpecBuilder) {
+				efsb.Insert("0", ssb.ExploreAll(ssb.Matcher()))
+				//efsb.Insert("0", ssb.Matcher())
+		}))).Node()
+
 }
 
 // map 
