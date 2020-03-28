09b926283b9d301c94629a86b444f10b77919a3a
hankelc <taichen.liu@imorpheus.io>
2020-03-28 09:27:57 +0800

    modify selector for real data test

--- cmd/tau-graphsync/createselector.go
+++ cmd/tau-graphsync/createselector.go
@@ -4,12 +4,16 @@ import (
 	"github.com/ipld/go-ipld-prime/traversal/selector/builder"
 	ipld     "github.com/ipld/go-ipld-prime"
 	ipldfree "github.com/ipld/go-ipld-prime/impl/free"
+	selectorbuilder "github.com/ipld/go-ipld-prime/traversal/selector/builder"
 )
 
 // state hamt tree
 func stateSelector() ipld.Node {
 	ssb := builder.NewSelectorSpecBuilder(ipldfree.NodeBuilder())
-    return ssb.ExploreAll(ssb.Matcher()).Node()
+    //return ssb.ExploreAll(ssb.Matcher()).Node()
+    return ssb.ExploreAll(ssb.ExploreFields(func(efsb selectorbuilder.ExploreFieldsSpecBuilder) {
+		efsb.Insert("0", ssb.Matcher())
+	})).Node()
 }
 
 // map 
