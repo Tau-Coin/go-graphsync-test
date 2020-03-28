a1e4b96ced8ade5c812ed46b798241d1f59a288e
shengxianshui <shengxianshui@imorpheus.io>
2020-03-28 16:19:02 +0800

    Try hamt selector

--- cmd/tau-graphsync/createselector.go
+++ cmd/tau-graphsync/createselector.go
@@ -4,13 +4,15 @@ import (
 	"github.com/ipld/go-ipld-prime/traversal/selector/builder"
 	ipld     "github.com/ipld/go-ipld-prime"
 	ipldfree "github.com/ipld/go-ipld-prime/impl/free"
-	selectorbuilder "github.com/ipld/go-ipld-prime/traversal/selector/builder"
+	ipldselector "github.com/ipld/go-ipld-prime/traversal/selector"
+	//selectorbuilder "github.com/ipld/go-ipld-prime/traversal/selector/builder"
 )
 
 // state hamt tree
 func stateSelector() ipld.Node {
 	ssb := builder.NewSelectorSpecBuilder(ipldfree.NodeBuilder())
     //return ssb.ExploreAll(ssb.Matcher()).Node()
+    /*
     return ssb.ExploreIndex(1, ssb.ExploreFields(func(efsb selectorbuilder.ExploreFieldsSpecBuilder) {
 		//efsb.Insert("0", ssb.ExploreAll(ssb.Matcher()))
 		//efsb.Insert("0", ssb.Matcher())
@@ -18,6 +20,10 @@ func stateSelector() ipld.Node {
 			efsb1.Insert("/", ssb.ExploreAll(ssb.Matcher()))
 		}))
 	})).Node()
+	*/
+
+	return ssb.ExploreRecursive(ipldselector.RecursionLimitNone(),
+		ssb.ExploreAll(ssb.ExploreRecursiveEdge())).Node()
 }
 
 // map 
